6cc9d7553dffff797a297383c985088daafe1888
bushishanguu <1750169605@qq.com>
2019-07-31 13:34:00 +0800

    将三个数从大到小排序

---
+++ 将三个数从大到小排序/将三个数从大到小排序/test.c
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include "stdio.h"
+#include "stdlib.h"
+
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	int t = 0;
+	printf("请输入三个数字 ");
+	scanf("%d %d %d", &a, &b, &c);
+	if (a < b)
+	{
+		t = a; a = b; b = t;
+	}
+	if (a < c)
+	{
+		t = a; a = c; c = t;
+	}
+	if (b < c)
+	{
+		t = b; b = c; c = t;
+	}
+	printf("%d %d %d\n", a, b, c);
+
+
+	system("pause");
+	return 0;
+}
