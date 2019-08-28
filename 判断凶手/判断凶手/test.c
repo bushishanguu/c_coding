72ac46505ac40d4577c8f7df01682dfe485a69a5
bushishanguu <1750169605@qq.com>
2019-08-28 10:18:49 +0800

    判断凶手

---
+++ 判断凶手/判断凶手/test.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include "stdio.h"
+#include "stdlib.h"
+//日本某地发生了一件谋杀案，警察通过排查确定杀人凶手必为4个
+//嫌疑犯的一个。以下为4个嫌疑犯的供词。
+//A说：不是我。
+//B说：是C。
+//C说：是D。
+//D说：C在胡说
+//已知3个人说了真话，1个人说的是假话。
+//现在请根据这些信息，写一个程序来确定到底谁是凶手。
+int main()
+{
+	char killer;
+	for (killer = 'A'; killer <= 'Z'; killer++)//利用ASCII码值  进行循环比较
+	{
+		if (((killer != 'A') + (killer == 'C') + (killer == 'D') + (killer != 'D') == 3)) //1+1+0+1=3
+		{
+			printf("%c是凶手\n", killer);
+			break;
+		}
+	}
+	system("pause");
+	return 0;
+}
