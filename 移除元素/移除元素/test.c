a03069470f8601b6285e721eb0e200230f20d574
bushishanguu <1750169605@qq.com>
2019-10-09 16:54:56 +0800

    给定一个数组 nums 和一个值 val，你需要原地移除所有数值等于 val 的元素，返回移除后数组的新长度。
    不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
    
    元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。

---
+++ 移除元素/移除元素/test.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+int removeElement(int* nums, int numsSize, int val){
+	if (numsSize == 0)
+		return 0;
+
+	// 声明头尾指针
+	int * left = nums;
+	int * right = nums + numsSize - 1;
+	while (left < right) {
+		// 从左向右找到等于 val 的位置
+		while (*left != val && left < right)
+			left++;
+		// 从右向左找到不等于 val 的位置
+		while (*right == val && left < right)
+			right--;
+		// 用右指针的元素覆盖左指针元素
+		*left = *right;
+		right--;
+	}
+	// 如果左指针的元素不是 val，则包括左指针处的元素
+	return left - nums + (*left != val);
+}
